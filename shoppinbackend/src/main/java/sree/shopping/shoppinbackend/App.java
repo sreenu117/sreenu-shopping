b0e24d845aa8c219d22862555c7953a6e9a275bc
Boggula Sreenu <javadeveloper4545@gmil.com>
2017-11-22 23:07:57 +0530

    My Project Successfully started

---
+++ shoppinbackend/src/main/java/sree/shopping/shoppinbackend/App.java
@@ -0,0 +1,13 @@
+package sree.shopping.shoppinbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
